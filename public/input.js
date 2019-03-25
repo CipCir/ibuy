4cceae5b0c2f8599538036baa3d573e2f65356d2
Ramo <ramona.miuta@gmail.com>
2019-03-25 18:41:53 +0200

    25.03.2019kitty

--- public/input.js
+++ public/input.js
@@ -1,33 +1,28 @@
 var inputObj = {
-    generalInfo:{
-        skin:"Amazon",// "Market place" "Custom",     
-        def:{
-            "Amazon":{
-                hasSearchBar:true,
-                prop:false
-            },
-            "Market place":{
-                hasSearchBar:false,
-                prop:true
-            },
-            "Custom":{
-                hasSearchBar:false,
-                prop:true
-            }
-        }
-    },    
-    layout:{
-         header:{
-
-         },
-         footer:{
-
-         }
+  generalInfo: {
+    skin: "Amazon", // "Amazon" "MarketPlace" "Custom",
+    skinProps: {
+      logo_img: "./img/emag_logo_150x50.png",
+      bk_color: "#005eb8",
+      cart_color: "white",
+      show_search: true
     },
-    texts:{
-        lvl_Main:{
-           subTitle:"All categories" 
-        }
-
+    texts: {
+      opt0: "<span style='color:white;'>Optional 0<br>Second row</span>",
+      opt1: "<span style='color:white;'>Optional 1</span>",
+      opt2: "<span style='color:white;'>Optional 2</span>",
+      opt3: "<span style='color:white;'>Optional 3</span>",
+      search_all_text: "All▾",
+      placeholder_search: "Placeholder text here..."
     }
-}
+  },
+  layout: {
+    header: {},
+    footer: {}
+  },
+  texts: {
+    lvl_Main: {
+      subTitle: "All categories"
+    }
+  }
+};
--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,48 @@
 <template>
   <div id="app">
     <headerComp
-      :skin="setup.skin"
+      :texts="inputObj.generalInfo.texts"
+      :skin="inputObj.generalInfo.skin"
+      :skinProps="inputObj.generalInfo.skinProps"
+      :nr_cart="5"
     />
-    <lvlMain
-      v-if="controls.showLevel=='Main'"
-      :texts=inputObj.texts.lvl_Main
-    />
+    <lvlMain v-if="controls.showLevel=='Main'" :texts="inputObj.texts.lvl_Main"/>
 
     <footerComp/>
   </div>
 </template>
 
 <script>
-import headerComp from './components/headerComp.vue'
-import lvlMain from './components/lvl_Main.vue'
-import footerComp from './components/footerComp.vue'
+import headerComp from "./components/headerComp.vue";
+import lvlMain from "./components/lvl_Main.vue";
+import footerComp from "./components/footerComp.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     headerComp,
     footerComp,
     lvlMain
   },
-  data(){
-    return{
+  data() {
+    return {
       inputObj,
-      output:{},
-      setup:{
-        skin:"Amazon"
-      },
-      controls:{
-        showLevel:"Main", // "Aisle","Shelf","Product","Enlarge","Cart"
+      output: {},
+      controls: {
+        showLevel: "Main" // "Aisle","Shelf","Product","Enlarge","Cart"
       }
-    }
-  },
+    };
+  }
   // created(){
   //   console.log(inputObj)
   // }
-
-}
+};
 </script>
 <style>
-#app{
-  width: 100%
+#app {
+  width: 100%;
 }
-.lvl_container{
-  width: 100%
+.lvl_container {
+  width: 100%;
 }
 </style>
