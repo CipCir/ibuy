2e23285f1e689edcf078e09ba116be4081cacc98
Cip cir <cipcir@gmail.com>
2019-03-25 12:16:35 +0200

    initial build

---
+++ public/input.js
@@ -0,0 +1,30 @@
+var inputObj = {
+    generalInfo:{
+        skin:"Amazon",// "Market place" "Custom",     
+        def:{
+            "Amazon":{
+                hasSearchBar:true,
+                prop:false
+            },
+            "Market place":{
+                hasSearchBar:false,
+                prop:true
+            },
+            "Custom":{
+                hasSearchBar:false,
+                prop:true
+            }
+        }
+    },    
+    layout:{
+         header:{
+
+         },
+         footer:{
+
+         }
+    },
+    texts:{
+
+    }
+}
